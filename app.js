96e28e6822b6b2c658e76165b3b5498e15d40d41
sarajadidi <sararivkajadidi@gmail.com>
2023-11-07 07:18:13 -0800

    finished exercise

--- app.js
+++ app.js
@@ -40,29 +40,64 @@ while( i <= 100){
 }
 
 console.log("EXERCISE 3 DO/WHILE :\n==========\n");
-
+let x = 1;
 do{
-    if (i % 2 != 0){
-        console.log(i)
+    if (x % 2 != 0){
+        console.log(x);
     }
-    i++
-}while(i <= 100)
+    x++
+}while(x < 100);
 
 console.log("EXERCISE 3 While :\n==========\n");
 
-while(i <= 100 ){
-    if (i % 3 == 0 && i % 5 == 0){
+let a = 1;
+while(a <= 100 ){
+    if (a % 3 == 0 && a % 5 == 0){
         console.log("FIZZBUZZ")
     }
-    else if (i % 3 == 0){
+    else if (a % 3 == 0){
         console.log("FIZZ")
     }
-    else if (i % 5 == 0){
+    else if (a % 5 == 0){
         console.log("BUZZ")
     }
     else{
-        console.log(i)
+        console.log(a)
     }
-    i++
+    a++
 }
 
+console.log("EXERCISE 3 Do/While :\n==========\n");
+
+let z = 1
+do{
+    if (z % 3 == 0 && z % 5 == 0){
+        console.log("FIZZBUZZ")
+    }
+    else if (z % 3 == 0){
+        console.log("FIZZ")
+    }
+    else if (z % 5 == 0){
+        console.log("BUZZ")
+    }
+    else{
+        console.log(z)
+    }
+    z++
+} while(z <= 100)
+
+
+console.log("EXERCISE 4 :\n==========\n");
+
+let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+for(let i = 1; i <= n; i++){
+    if(i == value){
+        console.log(`Found ${value}!`)
+        break;
+    }
+    if(i == n) {
+        console.log(`Did not find ${value} within 1-${n}.`)
+    }
+    
+}
